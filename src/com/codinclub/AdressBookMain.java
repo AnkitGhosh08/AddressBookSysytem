ef8834f6fdecb2e5e53d610a4fb52c63e6464c01
Ankit <ankitGhosh456@gmail.com>
2022-10-01 15:07:42 +0530

    ADDED: Add Contact

---
+++ src/com/codinclub/AdressBookMain.java
@@ -0,0 +1,9 @@
+package com.codinclub;
+
+public class AdressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+    }
+}
